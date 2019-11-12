054f182585ba5d7498d5234c790c3c71e7f88128
Sunny Singh <sunny@localhost.localdomain>
2019-11-12 15:40:26 +0530

    added after filter assign role

--- app/models/commitee_member.rb
+++ app/models/commitee_member.rb
@@ -1,4 +1,21 @@
 class CommiteeMember < ApplicationRecord
   belongs_to :commitee
   belongs_to :user
+
+  after_create :assign_role
+
+  def assign_role
+    level = self.commitee.level
+    if level == "Level 1"
+      user = self.user
+      user.role_masters << RoleMaster.find_by(name: 'l1')
+      user.role_masters << RoleMaster.find_by(name: 'l1_rep')
+      user.save
+    elsif level == "Appex"
+      user = self.user
+      user.role_masters << RoleMaster.find_by(name: 'l2')
+      user.role_masters << RoleMaster.find_by(name: 'l2_rep')
+      user.save
+    end
+  end
 end
