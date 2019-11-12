b051a274abdf1bb1b7399f9425f59e97a832c3e4
Sunny Singh <sunny@localhost.localdomain>
2019-11-12 12:13:15 +0530

    Added issue solved

--- db/migrate/20191105121520_create_rating_scales.rb
+++ db/migrate/20191105121520_create_rating_scales.rb
@@ -3,7 +3,7 @@ class CreateRatingScales < ActiveRecord::Migration[6.0]
     create_table :rating_scales do |t|
       t.string :title
       t.references :award_master, null: false, foreign_key: true
-      t.references :rating_scale, null: false, foreign_key: true
+      # t.references :rating_scale, null: false, foreign_key: true
       t.timestamps
     end
   end
