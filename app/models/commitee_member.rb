d96d27472205bfb41b1f2b8c0d0ed9f15e96babc
Sunny Singh <sunny@localhost.localdomain>
2019-11-20 16:51:39 +0530

    Remove bug

--- app/models/commitee_member.rb
+++ app/models/commitee_member.rb
@@ -6,12 +6,12 @@ class CommiteeMember < ApplicationRecord
 
   def assign_role
     level = self.commitee.level
-    if level == "Level 1"
+    if level.include?("Level 1")
       user = self.user
       user.role_masters << RoleMaster.find_by(name: 'l1')
       user.role_masters << RoleMaster.find_by(name: 'l1_rep')
       user.save
-    elsif level == "Appex"
+    elsif level.include?("Appex")
       user = self.user
       user.role_masters << RoleMaster.find_by(name: 'l2')
       user.role_masters << RoleMaster.find_by(name: 'l2_rep')
